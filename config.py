0e4b53a3c188b9acdd6d187d0c22a9b158d97e27
Jan Liby Dela Costa <janlibydelacosta@gmail.com>
2025-04-02 08:19:16 +0800

    fix: database unexpectedly closed connection error

--- config.py
+++ config.py
@@ -7,3 +7,17 @@ load_dotenv()
 class Config:
     SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "postgresql://user:password@localhost:5432/flask_db")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    
+    SQLALCHEMY_ENGINE_OPTIONS = {
+        'pool_size': 10,
+        'pool_recycle': 1800, 
+        'pool_pre_ping': True,
+        'pool_timeout': 30,
+        'max_overflow': 15,
+        'connect_args': {
+            'keepalives': 1,
+            'keepalives_idle': 30,
+            'keepalives_interval': 10,
+            'keepalives_count': 5
+        }
+    }
